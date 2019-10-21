646e7723fce4f190d623d0f85a161e51c35bef6b
GermainGit <prevotge@iut-acy.local>
2019-10-21 09:45:30 +0200

    git rm config

--- config.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 21 09:26:56 2019
-
-@author: prevotge
-"""
-
-amqp_url='amqp://qiwsedps:WMXj527zSKlGKFACO2IRj_ZEr4LRU3jg@dove.rmq.cloudamqp.com/qiwsedps'
